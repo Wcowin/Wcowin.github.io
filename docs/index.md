41cf86b4e046ca5c1e3ddebabd3987abd98f8504
Wcowin <1135801806@qq.com>
2023-08-22 00:01:33 +0800

    8.22

--- docs/index.md
+++ docs/index.md
@@ -20,11 +20,45 @@ comments: true
 
     - 搜索关键词以打开文章
     - 如遇到打开文章后无法显示图片的情况,请使用科学上网 -->
-<link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css">
+<!-- <link rel="stylesheet" href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css"> -->
+
 <div id="rcorners2">
-<div id="rcorners1" >
-<i class="fa fa-calendar" style="font-size:100%"></i>
-<b><span id="time"></span></b>
+<div id="rcorners1">
+<!-- <i class="fa fa-calendar" style="font-size:100%"></i> -->
+<body>
+    <p class="p1"></p>
+    <script>
+        //格式：2020年04月12日 10:20:00 星期二
+        function format(newDate) {
+            var day = newDate.getDay()
+            var y = newDate.getFullYear()
+            var m = (newDate.getMonth() + 1) < 10 ? "0" + (newDate.getMonth() + 1) : (newDate.getMonth() + 1)
+            var d = newDate.getDate() < 10 ? "0" + newDate.getDate() : newDate.getDate()
+            var h = newDate.getHours() < 10 ? "0" + newDate.getHours() : newDate.getHours()
+            var min = newDate.getMinutes() < 10 ? "0" + newDate.getMinutes() : newDate.getMinutes()
+            var s = newDate.getSeconds() < 10 ? "0" + newDate.getSeconds() : newDate.getSeconds()
+ 
+            var dict = {
+                1: "一",
+                2: "二",
+                3: "三",
+                4: "四",
+                5: "五",
+                6: "六",
+                0: "天",
+            }
+            //var week=["日","一","二","三","四","五","六"]
+            return y + "年" + m + "月" + d + "日" + h + ":" + min + ":" + s + " 星期" + dict[day]
+        }
+        
+        var timerId = setInterval(function(){
+            var newDate = new Date()
+          document.querySelector(".p1").textContent = format(newDate)
+        },1000)
+        
+  </script>
+</body>
+<!-- <b><span id="time"></span></b> -->
 
 </div>
 <ul>
@@ -58,4 +92,11 @@ comments: true
 
 
 
+
+
+ 
+
+ 
+
+ 
 
--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,33 +1,33 @@
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: true });
+// import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+// mermaid.initialize({ startOnLoad: true });
 
-function displayDate(){
-	document.getElementById("demo").innerHTML=Date();
-}
+// function displayDate(){
+// 	document.getElementById("demo").innerHTML=Date();
+// }
 
-// 时间
-function showTime() {
-  var date = new Date();
+// // 时间
+// function showTime() {
+//   var date = new Date();
 
-  var year = date.getFullYear();
-  var month = date.getMonth() + 1;
-  month = month < 10 ? "0" + month : month;
-  var day = date.getDate();
-  day = day < 10 ? "0" + day : day;
-  var week = "日一二三四五六".charAt(date.getDay()); // 使用charAt函数提取相应汉字
-  var hour = date.getHours();
-  hour = hour < 10 ? "0" + hour : hour; // 用三目运算符调整数字显示格式
-  var minute = date.getMinutes();
-  minute = minute < 10 ? "0" + minute : minute;
-  var second = date.getSeconds();
-  second = second < 10 ? "0" + second : second;
+//   var year = date.getFullYear();
+//   var month = date.getMonth() + 1;
+//   month = month < 10 ? "0" + month : month;
+//   var day = date.getDate();
+//   day = day < 10 ? "0" + day : day;
+//   var week = "日一二三四五六".charAt(date.getDay()); // 使用charAt函数提取相应汉字
+//   var hour = date.getHours();
+//   hour = hour < 10 ? "0" + hour : hour; // 用三目运算符调整数字显示格式
+//   var minute = date.getMinutes();
+//   minute = minute < 10 ? "0" + minute : minute;
+//   var second = date.getSeconds();
+//   second = second < 10 ? "0" + second : second;
 
-  var current = year + "-" + month + "-" + day + " " + "星期" + week + " " + hour + ":" + minute + ":" + second;
+//   var current = year + "-" + month + "-" + day + " " + "星期" + week + " " + hour + ":" + minute + ":" + second;
 
-  document.getElementById("time").innerHTML = current;
-}
+//   document.getElementById("time").innerHTML = current;
+// }
 
-setInterval("showTime()", 1000);
+// setInterval("showTime()", 1000);
 
 
 
@@ -670,3 +670,4 @@ if (isMobile) {
 } else {
   video.volume = 0; // 或者根据需要设置适当的音量值，例如 0.5 表示 50% 的音量
 }
+
